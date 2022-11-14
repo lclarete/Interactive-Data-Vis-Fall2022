81438c2f8a12685a7f799c83ade547cc6ed2dd11
lclarete <livia.clarete@gmail.com>
2022-11-14 00:07:14 -0500

    area chart

--- 2_3_time_series/NOTES.md
+++ 2_3_time_series/NOTES.md
@@ -1,5 +1,7 @@
 ## NOTES
 
+Line chart based on the Log GDP per capita of the US infrom 2006 to 2020. It was turned into an area chart by creating a new function (valueArea), with an additional axes: y1 containing the inner height of the y variable. 
+
 -----------
 INSTRUCTIONS:
 Use this markdown file to keep track of open questions/challenges from this week's assignment.
--- 2_3_time_series/main.js
+++ 2_3_time_series/main.js
@@ -15,6 +15,13 @@ var valueline = d3.line()
     .x(function(d) { return x(d.year); })
     .y(function(d) { return y(d.value); });
 
+var valueArea = d3.area()
+    .x(function(d) { return x(d.year); })
+    .y0(innerHeight)
+    .y1(function(d) { return y(d.value);
+    });
+
+
 // append the svg obgect to the body of the page
 // appends a 'group' element to 'svg'
 // moves the 'group' element to the top left margin
@@ -26,14 +33,16 @@ var svg = d3.select("body").append("svg")
           "translate(" + margin.left + "," + margin.top + ")");
 
 // Get the data
-d3.csv("https://raw.githubusercontent.com/lclarete/Interactive-Data-Vis-Fall2022/main/data/world-happiness-report.csv").then(function(data) {
+d3.csv("https://raw.githubusercontent.com/lclarete/Interactive-Data-Vis-Fall2022/main/data/world-happiness-report_us.csv").then(function(data) {
 
   // format the data
   data.forEach(function(d) {
       d.year = +(d.year);
-      d.Generosity = +d.Generosity;
+      d['Perceptions of corruption'] = +d['Perceptions of corruption'];
   });
-  var nest = d3.rollups(data, v => d3.mean(v, d => d.Generosity), d => d.year)
+  
+//   var nest = d3.rollups(data, v => d3.max(v, d => d['Log GDP per capita']), d => d.year);
+  var nest = d3.rollups(data, v => d3.min(v, d => d['Perceptions of corruption']), d => d.year)
   nest.sort(function(a, b){return a[0] - b[0]})
 
   var lineData = []
@@ -43,24 +52,22 @@ d3.csv("https://raw.githubusercontent.com/lclarete/Interactive-Data-Vis-Fall2022
 
   // Scale the range of the data
   x.domain(d3.extent(nest, function(d) { return d[0]; }));
-  y.domain([ d3.min(nest, function(d) { return d[1]; }), d3.max(nest, function(d) { return d[1]; })]);
+  y.domain([d3.min(nest, function(d) { return d[1]; }), d3.max(nest, function(d) { return d[1]; })]);
 
   // Add the valueline path.
   svg.append("path")
       .data([lineData])
       .attr("class", "line")
-      .attr("d", valueline)
-      .attr('fill' , 'none')
-      .attr('stroke' , 'black');
+      .attr("d", valueArea)
 
        // text label
        svg.append("text")
        .attr("transform", "rotate(-90)")
        .attr("y", 0 - margin.left)
        .attr("x",0 - (height / 2))
-       .attr("dy", "1em")
+       .attr("dy", "0.8em")
        .style("text-anchor", "middle")
-       .text("Generosity"); 
+       .text("Log GDP per capita"); 
    
        // text label for the x axis
        svg.append("text")             
@@ -73,11 +80,13 @@ d3.csv("https://raw.githubusercontent.com/lclarete/Interactive-Data-Vis-Fall2022
 
   // Add the x Axis
   svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
+    .attr('class', 'xaxis')
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
 
   // Add the y Axis
   svg.append("g")
-      .call(d3.axisLeft(y));
+  .attr('class', 'yaxis')
+  .call(d3.axisLeft(y));
 
 });
