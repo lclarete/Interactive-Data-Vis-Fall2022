99fa0aa516b79c0d6a74ca77d54c3affd37a2460
lclarete <livia.clarete@gmail.com>
2022-11-13 19:10:34 -0500

    replacing map.json stored in /data to one stored on github

--- 2_4_geographic/main.js
+++ 2_4_geographic/main.js
@@ -23,7 +23,7 @@ const colorScale = d3.scaleThreshold()
 
 // Load external data and boot
 Promise.all([
-d3.json("/data/map.geojson"), //map data
+d3.json("https://raw.githubusercontent.com/lclarete/Interactive-Data-Vis-Fall2022/main/data/map.geojson"), //map data
 d3.csv("https://raw.githubusercontent.com/lclarete/Interactive-Data-Vis-Fall2022/main/data/world-happiness-report.csv", function(d) {
   // console.log('hello', d)
     data.set(d['Country name']
